933331bf12c909a06b8fe5f3e8c5768a7388972e
rlhw9764 <rlhw9764@rennes.ft.fr>
2013-02-15 17:21:50 +0100

    Init OpenWeatherMapController

---
+++ MyCellarConditions/grails-app/controllers/com/mycellar/conditions/OpenWeatherMapController.groovy
@@ -0,0 +1,15 @@
+package com.mycellar.conditions
+
+class OpenWeatherMapController {
+
+    def index() { }
+	
+	def requestOWM() {
+
+		withRest(url: 'http://api.openweathermap.org/data/2.1/') {
+			def response = get(path: '/weather/city/3032965', query: [units: 'metric'])
+			println response.json
+		}
+	}
+	
+}
