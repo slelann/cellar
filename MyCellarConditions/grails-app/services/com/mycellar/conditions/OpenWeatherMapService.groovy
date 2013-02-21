a9d0b93921508c19367354b382a3c2fa1a73eb24
Sylvain <Sylvain@ASUS-PC.home>
2013-02-21 22:48:05 +0100

    Transform OpenWeatherMap client into service

---
+++ MyCellarConditions/grails-app/services/com/mycellar/conditions/OpenWeatherMapService.groovy
@@ -0,0 +1,26 @@
+package com.mycellar.conditions
+
+import org.codehaus.groovy.grails.web.json.JSONObject
+import org.codehaus.groovy.grails.commons.DefaultGrailsApplication
+
+import grails.converters.JSON
+import wslite.rest.*
+
+class OpenWeatherMapService {
+	
+	def grailsApplication = new DefaultGrailsApplication()
+	
+	def parseJsonResponse(String text) {
+		def json = JSON.parse(text)
+	}
+	
+	JSONObject getWeatherByCity() {
+		def restClient = new RESTClient(grailsApplication.config.openweathermap.service.urlClient)
+		def response = restClient.get(path:grailsApplication.config.openweathermap.service.pathWeather,
+										accept: ContentType.JSON,
+										query: [units: "metric"])
+		def json = JSON.parse(response.text)
+		return json
+	}
+
+}
---
+++ MyCellarConditions/test/unit/com/mycellar/conditions/OpenWeatherMapServiceTests.groovy
@@ -0,0 +1,17 @@
+package com.mycellar.conditions
+
+
+
+import grails.test.mixin.*
+import org.junit.*
+
+/**
+ * See the API for {@link grails.test.mixin.services.ServiceUnitTestMixin} for usage instructions
+ */
+@TestFor(OpenWeatherMapService)
+class OpenWeatherMapServiceTests {
+
+    void testSomething() {
+        fail "Implement me"
+    }
+}
