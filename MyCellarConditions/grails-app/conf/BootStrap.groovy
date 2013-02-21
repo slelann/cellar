1a6294e9135e335d6be41661bc2c8d5d83ff9fb8
slelann <slelann@gmail.com>
2013-02-21 10:19:00 +0100

    Update MyCellarConditions/grails-app/conf/BootStrap.groovy
    bootstrap data for series

--- MyCellarConditions/grails-app/conf/BootStrap.groovy
+++ MyCellarConditions/grails-app/conf/BootStrap.groovy
@@ -5,6 +5,7 @@ import com.mycellar.conditions.user.UserRole;
 class BootStrap {
 
     def init = { servletContext ->
+    		//ROLES+USERS
 		def adminRole = new Role(authority: 'ROLE_ADMIN').save(flush: true) 
 		def userRole = new Role(authority: 'ROLE_USER').save(flush: true)
 		def testUser = new User(username: 'me', enabled: true, password: 'password') 
@@ -13,6 +14,11 @@ class BootStrap {
 		assert User.count() == 1 
 		assert Role.count() == 2 
 		assert UserRole.count() == 1
+	    	//SERIES
+		def serieCSV = new Serie(serieName: 'Temperature/Humidite Cave', place: 'Maison' ).save(flush: true)
+		def serieExt = new Serie(serieName: 'Temperature/Humidite Exterieure', place: 'Chevaigne').save(flush: true)
+		assert Serie.count() == 2
+        
     }
 	
     def destroy = {
